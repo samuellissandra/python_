735ace8d9d56921dd6cc0a86cdef03dc8560d7be
Samuel Lauriano <samuellissandra@gmail.com>
2023-11-29 07:19:30 -0300

    UTF-8

---
+++ aulas/codificado_UTF-8.py
@@ -0,0 +1,12 @@
+"""
+Para declarar uma codificação diferente da padrão, uma linha de comentário especial deve ser adicionado como a primeira linha do arquivo. A sintaxe é a seguinte:
+
+# -*- coding: encoding -*-
+"""
+
+# Por exemplo, para declarar que a codificação do UTF-8 deve ser usada, o primeiro linha do seu arquivo de código-fonte deve ser: #-*- coding: UTF-8 -*- 
+
+# -*- coding: UTF-8 -*- 
+
+
+print('ofereça')
