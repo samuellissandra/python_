8ba469f39fdc4d220ace15278c783df8bb25a387
Samuel Lauriano <samuellissandra@gmail.com>
2023-11-29 07:37:19 -0300

    comentário

---
+++ aulas/comentarios.py
@@ -0,0 +1,10 @@
+# este é o primeiro comentário
+PI = 3.14 # este é o segundo comentário
+#... e agora um terceiro!
+text = "# Isto não é um comentário porque está entre aspas."
+
+"""
+samuel lauriano 
+surubim-pe
+um comentário de varias linhas
+entre asplas triplas """
