281ea39323c2a135f7e30fd16a81feb6e6932b8a
Samuel Lauriano <samuellissandra@gmail.com>
2023-11-29 07:54:04 -0300

    python como calculadora

---
+++ aulas/python_como_calculadora.py
@@ -0,0 +1,28 @@
+soma = 2 + 2
+print(soma)
+subtrair = 50 - 5*6
+print(subtrair)
+subtrair_multiplica_dividi = (50 - 5*6) / 4
+print(subtrair_multiplica_dividi)
+divisao = 8/5 # divisão sempre retorna um número de ponto flutuante
+print(divisao)
+
+"""
+17 / 3 # divisão clássica retorna um float
+res --> 5.666666666666667
+
+17 // 3 # divisão de piso descarta a parte fracionária
+res --> 5
+17 % 3 # o operador % retorna o restante da divisão
+res --> 2
+5 * 3 + 2 # quociente mínimo * divisor + resto
+res --> 17
+5 ** 2 # 5 ao quadrado
+res --> 25
+2 ** 7 # 2 elevado a 7
+res --> 128
+"""
+
+largura = 20
+altura = 5 * 9
+print(f'{largura} , {altura}')
