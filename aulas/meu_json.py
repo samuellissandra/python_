4e5fad14303cf7508708ec37d08f34f0c5f4e20e
Samuel Lauriano <samuellissandra@gmail.com>
2023-12-04 13:51:18 -0300

    Não pode criar o arquivo Json.py com o mesmo nome do pacote Json da um erro.

---
+++ aulas/meu_json.py
@@ -0,0 +1,9 @@
+import json
+
+# Não pode criar o arquivo Json.py com o mesmo nome do pacote Json da um erro.
+
+# Abre o arquivo JSON em modo de leitura
+with open(r"C:\Users\samue\OneDrive\Documentos\GitHub\python_\aulas\resultados.json", "r") as arq:
+    # Carrega o conteúdo do arquivo JSON para a variável 'valor'
+    valor = json.load(arq)
+    print(json.dumps(valor, indent=4))  # Imprime os dados com formatação usando json.dumps
