c2aaa4c8c7e6473ff09d32ceb7f1a1aa24ebedd2
Samuel Lauriano <samuellissandra@gmail.com>
2023-12-04 18:11:46 -0300

    Update meu_json.py

--- aulas/meu_json.py
+++ aulas/meu_json.py
@@ -6,4 +6,9 @@ import json
 with open(r"C:\Users\samue\OneDrive\Documentos\GitHub\python_\aulas\resultados.json", "r") as arq:
     # Carrega o conteúdo do arquivo JSON para a variável 'valor'
     valor = json.load(arq)
-    print(json.dumps(valor, indent=4))  # Imprime os dados com formatação usando json.dumps
+    # print(json.dumps(valor, indent=4))  # Imprime os dados com formatação usando json.dumps
+    for dados in valor:
+        print(dados['dados'])
+
+
+
