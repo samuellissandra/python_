57a1b81df8e2f8cf18399582f43aa85907f8769f
Samuel Lauriano <samuellissandra@gmail.com>
2023-12-04 12:52:09 -0300

    conforme

--- aulas/json.py
+++
@@ -1,7 +0,0 @@
-import json
-
-# -*- coding: utf-8 -*-
-
-with open(r'C:\Users\samue\OneDrive\Documentos\GitHub\python_\aulas\resultados.json','r') as arq:
-    json_arq = json.load(arq)
-    print(json_arq)
