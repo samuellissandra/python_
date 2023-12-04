b648e6d902e64d0fab506097c457129a1cc09044
Samuel Lauriano <samuellissandra@gmail.com>
2023-12-04 12:49:28 -0300

    Update json.py

--- aulas/json.py
+++ aulas/json.py
@@ -2,8 +2,6 @@ import json
 
 # -*- coding: utf-8 -*-
 
-"""with open('resultados.json','r') as arq:
+with open(r'C:\Users\samue\OneDrive\Documentos\GitHub\python_\aulas\resultados.json','r') as arq:
     json_arq = json.load(arq)
-    print(json_arq)"""
-
-print('ok')
+    print(json_arq)
