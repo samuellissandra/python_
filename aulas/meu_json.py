d9f6b5314508ff32cff700f3ec75b68cb8e28fba
Samuel Lauriano <samuellissandra@gmail.com>
2023-12-04 22:00:19 -0300

    Update meu_json.py

--- aulas/meu_json.py
+++ aulas/meu_json.py
@@ -1,14 +1,21 @@
+
+
+
 import json
 
-# Não pode criar o arquivo Json.py com o mesmo nome do pacote Json da um erro.
+cont = 0
+meus = {1, 2, 3, 4}
+novo = {}
 
 # Abre o arquivo JSON em modo de leitura
 with open(r"C:\Users\samue\OneDrive\Documentos\GitHub\python_\aulas\resultados.json", "r") as arq:
     # Carrega o conteúdo do arquivo JSON para a variável 'valor'
-    valor = json.load(arq)
-    # print(json.dumps(valor, indent=4))  # Imprime os dados com formatação usando json.dumps
-    for dados in valor:
-        print(dados['dados'])
-
-
-
+    arquivo = json.load(arq)
+    
+    for dados in arquivo:
+        for num in dados['dados']:
+            novo = int(num)
+            meus.add(novo)
+            cont += 1
+        
+print(cont)
